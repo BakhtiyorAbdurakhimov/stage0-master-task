bebdbe6a7493368fbb9d5739246f6e04a82f084a
Bakhtiyor22 <baxtiyorabduraximov2203@gmail.com>
2025-02-14 21:28:22 +0500

    Completed stage0

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,12 @@
 package com.epam.langSyntax;
 
 public class NumberReverter {
-
     public void revert(int number) {
+        int reversedNumber = 0;
+        while (number != 0) {
+            reversedNumber = reversedNumber * 10 + number % 10;
+            number /= 10;
+        }
+        System.out.println(reversedNumber);
     }
-
 }
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -1,13 +1,22 @@
 package com.epam.loops;
 
 public class Pyramid {
-
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            for (int l = i ; l > 1; l--) {
+                System.out.print(l);
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
-
 }
